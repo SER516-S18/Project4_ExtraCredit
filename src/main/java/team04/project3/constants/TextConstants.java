0a8113ead9c747e900af00c1c5b0479c47f725e1
abhis1989kumar <abhis.kumar99@gmail.com>
2018-04-28 00:49:29 -0700

    Updating comments

--- src/main/java/team04/project3/constants/TextConstants.java
+++ src/main/java/team04/project3/constants/TextConstants.java
@@ -7,7 +7,7 @@ import java.awt.*;
  */
 
 public interface TextConstants {
-    public static final Font LARGE_FONT = new Font("Monospaced", Font.PLAIN, 20);
+    public static final Font LARGE_FONT = new Font("Monospaced", Font.PLAIN, 20);//setting the LARGE_FONT style 
     public static final Font DEFAULT_FONT = new Font("Monospaced", Font.PLAIN, 16);
     public static final Font SMALL_FONT = new Font("Monospaced", Font.PLAIN, 13);
 }
