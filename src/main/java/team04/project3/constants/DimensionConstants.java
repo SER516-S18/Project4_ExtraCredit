b8da1d03db60e4ba60b0d4af7302e02adcf086ae
Rachana Nagaraj Kashyap <rach.kashyap@gmail.com>
2018-04-28 00:43:44 -0700

    Updating the comments of constants

--- src/main/java/team04/project3/constants/DimensionConstants.java
+++ src/main/java/team04/project3/constants/DimensionConstants.java
@@ -6,10 +6,10 @@ public interface DimensionConstants {
     public static final int EYE_RADIUS = 5;  //setting the EYE_RADIUS to 5
     public static final int LEFT_EYE_X_POSITION = 34; //setting the LEFT_EYE_X_POSITION to 34
     public static final int RIGHT_EYE_X_POSITION = 59;//setting the RIGHT_EYE_X_POSITION to 59
-    public static final int EYE_Y_POSITION = 20;
-    public static final int PUPIL_SIZE = 3;
-    public static final int EYE_BROW_LEFT_X1 = 32;
-    public static final int EYE_BROW_LEFT_X2 = 45;
+    public static final int EYE_Y_POSITION = 20;//setting the EYE_Y_POSITION to 20
+    public static final int PUPIL_SIZE = 3;//setting the PUPIL_SIZE to 3
+    public static final int EYE_BROW_LEFT_X1 = 32;//setting the EYE_BROW_LEFT_X1 to 32
+    public static final int EYE_BROW_LEFT_X2 = 45;//setting the EYE_BROW_LEFT_X2 to 45
     public static final int EYE_BROW_RIGHT_X1 = 56;
     public static final int EYE_BROW_RIGHT_X2 = 68;
     public static final int EYE_BROW_Y_POSITION = 12;
@@ -18,8 +18,8 @@ public interface DimensionConstants {
     public static final int NOSE_Y1_POSITION = 27;
     public static final int NOSE_WIDTH = 8;
 
-    public static final int FACE_X_COORDINATE = 37;
-    public static final int FACE_Y_COORDINATE = 27;
+    public static final int FACE_X_COORDINATE = 37;//setting the FACE_X_COORDINATE to 37
+    public static final int FACE_Y_COORDINATE = 27;//setting the FACE_Y_COORDINATE to 27
 
     public static final int LOOK_LEFT_RIGHT_VAL = 2;
 }
